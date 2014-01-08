5aea7d5be3150fb84a13ab4c38b580d60989ed52
Karthik Bala <drmelonhead@gmail.com>
2014-01-07 23:19:58 -0800

    threading is slower idk what i'm doing

--- chapter1/zipper.py
+++ chapter1/zipper.py
@@ -1,23 +1,37 @@
 import zipfile
-from threading import thread
-import optparse
+from threading import Thread
+import timeit
 
 def extractFile(zFile, password):
 	try:
 		zFile.extractall(pwd=password)
-		return password
+		#return password
+		print "password is " + password
 	except Exception, e:
-		return
+		#return
+		pass
 
 
-def main():
+# def main(): 15.24 sec runtime
+# 	zFile = zipfile.ZipFile("evil.zip")
+# 	passFile = open('longdict.txt')
+# 	for line in passFile.readlines():
+# 		password = line.strip("\n")
+# 		#t = Thread(target=extractFile, args=(zFile,password))
+# 		#t.start()
+# 		guess = extractFile(zFile, password)
+# 		if guess:
+# 			print '[+] Password is ' + password
+
+def main(): #32.3 s runtime?
 	zFile = zipfile.ZipFile("evil.zip")
-	passFile = open('dictionary.txt')
+	passFile = open('longdict.txt')
 	for line in passFile.readlines():
 		password = line.strip("\n")
-		guess = extractFile(zFile, password)
-		if guess:
-			print '[+] Password is ' + password
-			exit(0)
+		t = Thread(target=extractFile, args=(zFile,password))
+		t.start()
 
+start = timeit.default_timer()
 main()
+stop = timeit.default_timer()
+print "Finished in %f seconds." % (stop - start)
