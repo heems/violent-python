d307c9422c74e2f5205878c18a44578b0ae63695
Karthik Bala <drmelonhead@gmail.com>
2014-01-12 14:18:56 -0800

    threading

--- chapter2/scanner.py
+++ chapter2/scanner.py
@@ -1,21 +1,31 @@
 from socket import *
 import socket
+from threading import *
 
-def conn_scan(port, host):
+screenlock = Semaphore(value=1)
+
+def conn_scan(host, port):
 	try:
 		test_socket = socket.socket(AF_INET, SOCK_STREAM)
 		test_socket.connect((host, port)) #higher level than socket.connect(), works for both AF_INET and AF_INET6
 		test_socket.send('thing')
 		results = test_socket.recv(128)
+		screenlock.acquire()
 		print '[+] Port %d open.' % port
 		print '[+] ' + str(results)
 	except socket.gaierror:
 		print 'Could not connect to host.'
 	except:
 		print '[-] Port %d closed.' % port
+	finally:
+		screenlock.release()
+		test_socket.close()
 
 host = raw_input("enter hostname (google.com): ")
 print("\n")
-port = int(raw_input("enter port numbers (1, 2, 3): "))
+ports = raw_input("enter port numbers (1, 2, 3): ").split(", ")
+print ports
 
-conn_scan(port, host)
+for port in ports:
+	t = Thread(target=conn_scan, args=(host, int(port)))
+	t.start()
