248a7bfa5aca96c2ac236deb2d32b4c10c57b1c7
Karthik Bala <drmelonhead@gmail.com>
2014-01-12 14:09:10 -0800

    finished i think, deviated from book

--- chapter2/scanner.py
+++ chapter2/scanner.py
@@ -1,32 +1,21 @@
 from socket import *
 import socket
 
-
-
 def conn_scan(port, host):
 	try:
 		test_socket = socket.socket(AF_INET, SOCK_STREAM)
 		test_socket.connect((host, port)) #higher level than socket.connect(), works for both AF_INET and AF_INET6
+		test_socket.send('thing')
+		results = test_socket.recv(128)
 		print '[+] Port %d open.' % port
+		print '[+] ' + str(results)
+	except socket.gaierror:
+		print 'Could not connect to host.'
 	except:
 		print '[-] Port %d closed.' % port
 
-
-
-
-# def port_scan(host, ports):
-# 	try:
-# 		tgtIP = gethostby
-
-
-
 host = raw_input("enter hostname (google.com): ")
-try:
-	host = int(host)
-except:
-	host = host
-
-print("\n\n")
+print("\n")
 port = int(raw_input("enter port numbers (1, 2, 3): "))
 
 conn_scan(port, host)
